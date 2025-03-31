ec7100430b73e70374db8c6f54cb797ba24a044c
Joseph Ajayi <ajayimiles73@gmail.com>
2025-04-01 00:13:26 +0100

    jwt update

--- src/tokens/jwt.js
+++ src/tokens/jwt.js
@@ -2,9 +2,9 @@ import jwt from 'jsonwebtoken';
 import { config } from '../config/env.js';
 
 export const aToken = (payload) => {
-    return jwt.sign(payload, config.asecret, {expiresIn: '1h'})
+    return jwt.sign(payload, config.asecret, {expiresIn: '15m'})
 };
 
 export const rToken = (payload) => {
-    return jwt.sign(payload, config.rsecret, {expiresIn: '30d'})
+    return jwt.sign(payload, config.rsecret, {expiresIn: '180d'})
 };
