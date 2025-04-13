94eec068d95b4ccee025365709fcf4927fc70ea9
Joseph Ajayi <ajayimiles73@gmail.com>
2025-04-13 11:37:48 +0100

    RT

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -30,8 +30,8 @@ export const loginUserController = async (req, res) => {
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true, // Prevents JavaScript access
-            secure: config.nodeEnv, // Ensures HTTPS-only (set false for local dev)
-            sameSite: config.nodeEnv ? "None" : "Lax", // Required for cross-origin requests
+            secure: config.nodeEnv === "production", // Ensures HTTPS-only (set false for local dev)
+            sameSite: config.nodeEnv === "production" ? "None" : "Lax", // Required for cross-origin requests
             path: "/",
             maxAge: 6 * 30 * 24 * 60 * 60 * 1000 // 6 months
         });
