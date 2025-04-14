084651eca03771ce1907a4e37aec7ba47be9af9d
Joseph Ajayi <ajayimiles73@gmail.com>
2025-04-14 09:17:26 +0100

    cook

--- src/config/database.js
+++ src/config/database.js
@@ -3,38 +3,16 @@ import { config } from './env.js';
 const { Pool } = pkg;
 
 
-// const getConnection = new Pool({
-//     connectionString: config.dburl,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
-// export const executeQuery = (query, values) => {
-//     return new Promise((resolve, reject) => {
-//         getConnection.query(query, values, (error, results) => {
-//             if(error) {
-//                 console.error(error);
-//                 return reject(error);
-//             }
-//             return resolve(results);
-//         })
-//     }) 
-// };
-
-const connection =  new Pool({
-
-    port: config.db.port,
-    user: config.db.user,
-    host: config.db.host,
-    database: config.db.name,
-    password: config.db.pass
-
+const getConnection = new Pool({
+    connectionString: config.dburl,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export const executeQuery = (query, values) => {
     return new Promise((resolve, reject) => {
-        connection.query(query, values, (error, results) => {
+        getConnection.query(query, values, (error, results) => {
             if(error) {
                 console.error(error);
                 return reject(error);
@@ -43,3 +21,25 @@ export const executeQuery = (query, values) => {
         })
     }) 
 };
+
+// const connection =  new Pool({
+
+//     port: config.db.port,
+//     user: config.db.user,
+//     host: config.db.host,
+//     database: config.db.name,
+//     password: config.db.pass
+
+// });
+
+// export const executeQuery = (query, values) => {
+//     return new Promise((resolve, reject) => {
+//         connection.query(query, values, (error, results) => {
+//             if(error) {
+//                 console.error(error);
+//                 return reject(error);
+//             }
+//             return resolve(results);
+//         })
+//     }) 
+// };
