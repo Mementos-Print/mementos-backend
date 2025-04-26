17392f28ca01003db03bb20a9b8f009cd3c45c43
Joseph Ajayi <ajayimiles73@gmail.com>
2025-04-26 14:50:31 +0100

    model

--- src/users/users.models.js
+++ src/users/users.models.js
@@ -8,7 +8,8 @@ export const createUserTable = async() => {
         userID VARCHAR(300) NOT NULL PRIMARY KEY,
         email VARCHAR(200) UNIQUE NOT NULL,
         name VARCHAR(100) NOT NULL,
-        role VARCHAR(50) DEFAULT('customer')
+        role VARCHAR(50) DEFAULT('customer'),
+        password VARCHAR(300) DEFAULT('no)
         );
         `;
 
