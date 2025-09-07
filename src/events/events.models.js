d5e477c217de4c610fd1186b2b2ee7a08e380d28
JosephMiless <107660136+JosephMiless@users.noreply.github.com>
2025-09-07 01:15:41 +0100

    Update events.models.js

--- src/events/events.models.js
+++ src/events/events.models.js
@@ -30,7 +30,7 @@ export const createEventUsersTable = async () => {
         id VARCHAR(300) NOT NULL PRIMARY KEY,
         userID VARCHAR(300) NOT NULL,
         eventID VARCHAR(300) NOT NULL,
-        joinedAt DATE NOT NULL,
+        joinedAt TIMESTAMP DEFAULT (TIMESTAMP),
         FOREIGN KEY(userID) REFERENCES users(userID) ON DELETE CASCADE,
         FOREIGN KEY(eventID) REFERENCES events(eventID) ON DELETE CASCADE
         );
